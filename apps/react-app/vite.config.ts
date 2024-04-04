ce8db6bfc4bda89ac0a0b318f846f4ee8c7e161c
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2024-04-04 17:42:01 +0300

    change configs

--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,49 +1,55 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/react-app',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const serverPort = Number(env.VITE_REACT_APP_PORT) || 4200;
+  const previewPort = Number(env.VITE_REACT_APP_PORT_PREVIEW) || 4300;
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/react-app',
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: '../../dist/apps/react-app',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+      port: serverPort,
+      host: 'localhost',
     },
-  },
 
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
+    preview: {
+      port: previewPort,
+      host: 'localhost',
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/react-app',
-      provider: 'v8',
+    plugins: [react(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+      outDir: '../../dist/apps/react-app',
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
-  },
+
+    test: {
+      globals: true,
+      cache: {
+        dir: '../../node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/react-app',
+        provider: 'v8',
+      },
+    },
+  };
 });
