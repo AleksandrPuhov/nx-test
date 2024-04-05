c87c36fc0dc8303621f67cd90dba36ba8469b85c
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2024-04-05 10:48:42 +0300

    config start in dev mode

--- apps/nest-back-app/src/main.ts
+++ apps/nest-back-app/src/main.ts
@@ -5,6 +5,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // const CORS_ORIGINS = configService.get('CORS_ORIGINS') || 'http://localhost:3000'
+  // const corsOrigins = CORS_ORIGINS.split(',')
+
+  app.enableCors({
+    // origin: corsOrigins,
+    credentials: true,
+    exposedHeaders: ['Content-Type', 'Content-Disposition'],
+  });
+
   const port = process.env.NEST_PORT || 3001;
 
   await app.listen(port);
